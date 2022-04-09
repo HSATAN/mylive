142d870eef074a48b249d713711bf59e7c7f470e
edison <2499090390@qq.com>
2022-04-09 11:34:04 +0800

    添加python需求文件

--- mylive/mylive/settings.py
+++ mylive/mylive/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^y9)kukz!m82*@3x22wl*t1)wr2pdd(&kgp&to9je=z9aj28%c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
